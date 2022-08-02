61b4ae00f2319bfc9f48d57c3077c0dff7e53bcb
yunho <Yunho7058@github.com>
2022-08-02 23:55:24 +0900

    filter

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 const HeaderBack = styled.div`
   position: fixed;
   top: 0px;
-  background-color: #d7ac87;
+  background-color: #ddd4c9;
   width: 100vw;
-  height: 50px;
+  height: 40px;
   z-index: 999;
   display: flex;
   justify-content: space-around;
   align-items: center;
+  cursor: pointer;
 `;
 
 function Header() {
